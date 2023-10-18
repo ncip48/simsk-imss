e0e3dfe74df2fe1ef0f9c9ca5dbf5fc084efec2e
ncip48 <mbahcip00@gmail.com>
2023-10-18 13:06:50 +0700

    ok

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -2,7 +2,7 @@
     <!-- /.login-logo -->
     <div class="card card-outline card-primary">
         <div class="card-header text-center">
-            <a href="#" class="h1"><b>SIM</b>Surat aowekwoaeo</a>
+            <a href="#" class="h1"><b>PT IMSS</b></a>
         </div>
         <div class="card-body">
             <p class="login-box-msg">Sign in to start your session</p>
